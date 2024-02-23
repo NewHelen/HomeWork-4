4d9ffabb7b4c799959be7a68e9da60e110820d4b
Olena Zakharina <strin000@gmail.com>
2024-02-23 23:04:51 +0200

    added comments

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -48,7 +48,7 @@ public class Run {
                 .stream()
                 .map(name -> name.toUpperCase())    // у верхньому регістрі
                 .sorted(Comparator.reverseOrder())  // відсортованих за спаданням
-                .collect(Collectors.toList());      // перетворити в список
+                .toList();      // перетворити в список
     }
 
     // task 3
@@ -56,9 +56,9 @@ public class Run {
        return array
                 .stream()
                 .flatMap(s -> Arrays.stream(s.split(", ")))
-                .map(Integer::parseInt)
+                .map(s1 -> Integer.parseInt(s1))  // перетворити String to Integer
                 .sorted()
-                .map(Object::toString)
+                .map(integer -> integer.toString())     // перетворити Integer to String
                 .collect(Collectors.joining(", "));
     }
 
@@ -69,12 +69,45 @@ public class Run {
 
     // task 5
     public static <T> Stream<T> zip(Stream<T> first, Stream<T> second){
-        List<T> firstList = first.collect(Collectors.toList());   // перетворити в список
-        List<T> secondList = second.collect(Collectors.toList());
+        List<T> firstList = first.toList();   // перетворити в список
+        List<T> secondList = second.toList();
 
         int size = Math.min(firstList.size(), secondList.size()); // визначити мінімальний розмір списків
 
         return Stream.iterate(0, i -> i < size, i -> i + 1)
                 .flatMap(i -> Stream.of(firstList.get(i), secondList.get(i)));
     }
+
+    /*
+       Intermediate operation
+
+     filter(Predicate<T> predicate) - if
+     map(Function<T, R> mapper) - перетворення
+     flatMap(Function<T, Stream<R>> mapper) -
+     distinct() - приберає дублікати
+     sorted() - сортування (ексепшен якщо не имплементить comparable)
+     sorted(Comparator<? super T> comparator) - свое порівняння обьектів
+     peek(Consumer<? super T> action) - наприклад логування
+     limit - (long maxSize) - вивиде обмежену кількість
+     skip - (long n) - пропустить елементи
+
+       Terminal operators
+
+     forEach(Consumer<? super T> action)
+     forEachOrdered(Consumer<? super T> action)
+     toArray()
+     reduce(BinaryOperator<T> accumulator)
+     collect(Collector<? super T,A,R> collector)
+     min(Comparator<? super T> comparator)
+     max(Comparator<? super T> comparator)
+     count()
+     anyMatch(Predicate<? super T> predicate)
+     allMatch(Predicate<? super T> predicate)
+     noneMatch(Predicate<? super T> predicate)
+     findFirst()
+     findAny()
+
+*/
+
+
 }
